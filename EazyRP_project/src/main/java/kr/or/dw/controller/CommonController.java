07757d491c309c2bb8e355a2c37e12ea43adac8b
jimin <netwlals1210@naver.com>
2023-07-03 12:00:48 +0900

    sdasd

---
+++ EazyRP_project/src/main/java/kr/or/dw/controller/CommonController.java
@@ -0,0 +1,19 @@
+package kr.or.dw.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CommonController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommonController.class);
+
+	@GetMapping("/common/loginForm")
+	public String loginForm() {
+		return "/common/loginForm";
+	}
+	
+}
