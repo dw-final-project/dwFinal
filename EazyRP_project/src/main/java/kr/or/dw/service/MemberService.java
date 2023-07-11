2b6898e3010a95f03df1a1ad73a2d521f7791556
chldmsql <126857666+chldmsql@users.noreply.github.com>
2023-07-11 12:26:08 +0900

    .

---
+++ EazyRP_project/src/main/java/kr/or/dw/service/MemberService.java
@@ -0,0 +1,11 @@
+package kr.or.dw.service;
+
+import java.sql.SQLException;
+
+import kr.or.dw.vo.MemberVO;
+
+public interface MemberService {
+
+	MemberVO selectMemberById(String id) throws SQLException;
+
+}
