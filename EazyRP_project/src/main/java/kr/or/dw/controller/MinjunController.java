c26dbcf83d7dd53ef5e669ff2962f897729e8b09
minjunkimmm <126761555+minjunkimmm@users.noreply.github.com>
2023-07-13 16:10:53 +0900

    d

--- EazyRP_project/src/main/java/kr/or/dw/controller/MinjunController.java
+++ EazyRP_project/src/main/java/kr/or/dw/controller/MinjunController.java
@@ -29,5 +29,54 @@ public class MinjunController {
 		return "minjun/main";
 	}
 	
+<<<<<<< Updated upstream
 	
+=======
+
+	@RequestMapping("/product")
+	public ModelAndView productMain(String mcode, ModelAndView mnv) throws SQLException {
+		String url = "minjun/product";
+		
+		mnv.addObject("mcode", mcode);
+		mnv.setViewName(url);
+		return mnv;
+	}
+
+	
+	@RequestMapping("/productRegistForm")
+	public String productRegistForm() {
+		String url = "minjun/product_open_regist";
+		return url;
+	}
+	
+
+	@RequestMapping("/order")
+	public ModelAndView orderMain(String mcode, ModelAndView mnv) throws SQLException {
+		String url = "minjun/order";
+		
+		
+		mnv.addObject("mcode", mcode);		
+		mnv.setViewName(url);
+		
+		return mnv;
+	}
+
+	
+	@RequestMapping("/orderRegistForm")
+	public String orderRegistForm() {
+		String url = "minjun/order_open_regist";
+		return url;
+	}
+	
+	@RequestMapping("/regist")
+	public void regist(ProcessVO processVo, HttpServletRequest req, HttpServletResponse res) throws SQLException, IOException {
+//		processService.registProcess(processVo);
+		res.setContentType("text/html; charset=utf-8");
+		PrintWriter out = res.getWriter();
+		out.println("<script>");
+		out.println("alert('성공적으로 등록되었습니다.')");
+		out.println("window.opener.location.reload(true); window.close();");
+		out.println("</script>");
+	}
+>>>>>>> Stashed changes
 }
