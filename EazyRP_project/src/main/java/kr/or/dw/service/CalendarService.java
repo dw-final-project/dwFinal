b0df1a8561f8bea0b2a9882dcd3a9a908d81680b
박희성 <126127732+bakisung@users.noreply.github.com>
2023-07-14 16:52:35 +0900

    .

---
+++ EazyRP_project/src/main/java/kr/or/dw/service/CalendarService.java
@@ -0,0 +1,14 @@
+package kr.or.dw.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import kr.or.dw.vo.CalendarVO;
+
+public interface CalendarService {
+	
+	List<CalendarVO> getCalendar() throws SQLException;
+
+	void registCalendar(CalendarVO calendar) throws SQLException;
+	
+}
